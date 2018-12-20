3d685be6b6d0d31a2a9d003af541eb19b9955125
Rebecca Frank <rebeccaffrank@gmail.com>
2018-12-20 16:59:09 -0600

    Add test case for count of 0

--- js/index.test.js
+++ js/index.test.js
@@ -20,6 +20,13 @@ const mockData = {
         aC: 46,
         aP: 622
     },
+    1908: {
+        C: 0,
+        P: 3,
+        S: 0,
+        aC: 46,
+        aP: 622
+    },
     // district with missing data
     1906: {
         P: 923
@@ -148,6 +155,16 @@ test('popup of non-errored district shows normal popup 3', () => {
         "Name</b>, the 43 <b>Black/African American Students</b> received " +
         "15.22% of the 46 <b>Out of School Suspensions</b> and represented " +
         "6.91% of the district population.</span>");
+});
+
+test('popup of non-errored district with count of 0 shows normal popup', () => {
+    M.processedData = mockData;
+    var layer = { bindPopup : jest.fn() };
+    M.getOptions().onEachFeature(mockFeature(1908), layer);
+    expect(layer.bindPopup).toBeCalledWith("<span class='popup-text'>In <b>Test District " +
+        "Name</b>, the 3 <b>Black/African American Students</b> received " +
+        "0% of the 46 <b>Out of School Suspensions</b> and represented " +
+        "0.48% of the district population.</span>");
 });
 
 test('popup of district that does not exist shows data not available error', () => {
